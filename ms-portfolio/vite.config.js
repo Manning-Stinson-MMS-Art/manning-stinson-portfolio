566d94db32f02202155ba2c255936eb8a8fb9cd0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-06 16:37:32 +0000

    updated vite config file

--- ms-portfolio/vite.config.js
+++ ms-portfolio/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Specify the output directory if you want to ensure it's correct
+    emptyOutDir: true, // Optional: clears the output directory before building
+  },
+  server: {
+    port: 3000, // You can set a custom port for development if needed
+  },
 })
