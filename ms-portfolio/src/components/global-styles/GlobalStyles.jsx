2dff81f1cd4d80a696b124877521e8a4d03a5b2f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-22 20:32:42 +0000

    updated imports for base.scss

--- ms-portfolio/src/components/global-styles/GlobalStyles.jsx
+++ ms-portfolio/src/components/global-styles/GlobalStyles.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 // Import all the necessary styles using the @styles alias
-import '@styles/main.scss';   // Main styles
+import '@styles/base.scss';   // Global styles
+import '@styles/reset.scss';   // Reset styles
+import '@styles/fonts.scss';   // Font styles
 import '@styles/typography.scss';   // Typography styles
 import '@styles/utilities.scss';   // Utility styles
 import '@styles/variables.scss';   // Variable styles
 import '@styles/animations.scss';   // Animation styles
-import '@styles/base.scss';   // Global styles
-import '@styles/reset.scss';   // Reset styles
 import '@styles/mixins.scss';   // Responsive styles
 
 
