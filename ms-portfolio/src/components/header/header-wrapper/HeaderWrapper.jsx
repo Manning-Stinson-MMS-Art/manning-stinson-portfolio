1b1de7dce8e10338ceb179b125781458dc46aedc
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-23 00:24:30 +0000

    updated file extension from css to scss

--- ms-portfolio/src/components/header/header-wrapper/HeaderWrapper.jsx
+++ ms-portfolio/src/components/header/header-wrapper/HeaderWrapper.jsx
@@ -1,7 +1,7 @@
 // src/components/header/header-wrapper/HeaderWrapper.jsx
 import React from 'react';
 import { LogoWrapper, MenuWrapper, SocialIcons } from './'; // Assuming you're in the same directory
-import './HeaderWrapper.css';
+import './HeaderWrapper.scss';
 
 const HeaderWrapper = () => {
   return (
