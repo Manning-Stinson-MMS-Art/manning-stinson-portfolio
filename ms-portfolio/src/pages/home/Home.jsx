e4ba2a825b626d0191c3bb08ef1aab1d1e2cf9aa
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-23 00:00:28 +0000

    updated home.jsx

--- ms-portfolio/src/pages/home/Home.jsx
+++ ms-portfolio/src/pages/home/Home.jsx
@@ -1,11 +1,9 @@
 // src/pages/home/Home.jsx
 import React from 'react';
-
 import GlobalStyles from '@components/global-styles/GlobalStyles';
 import ResponsiveStyles from '@components/responsive-styles/Responsive-Styles';
 import PageWrapper from '@components/page-wrapper/PageWrapper';
 import Header from '@components/header/Header'; // Import the Header component
-
 
 const Home = () => { // Renamed from HomePage to Home
     console.log("Home component rendered.");
@@ -16,7 +14,6 @@ const Home = () => { // Renamed from HomePage to Home
             <ResponsiveStyles />
             <PageWrapper>
                 <Header /> {/* Render the Header component here */}
-                <HeroHeaderImage /> {/* Ensure this component is defined */}
                 <h1>Hi. Welcome!</h1>
             </PageWrapper>
         </>
