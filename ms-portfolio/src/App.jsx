6b1332a9f8bf0d7e6a8ef3f568f8af9a191b9a6f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-30 14:53:58 +0000

    updated blog, contact, portfolio page with layout component

--- ms-portfolio/src/App.jsx
+++ ms-portfolio/src/App.jsx
@@ -1,23 +1,24 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '@pages/home/Home'; // Keep as Home
-import Contact from '@pages/contact/Contact'; 
-import Portfolio from './pages/portfolio/Portfolio';
+import Home from '@pages/home/Home';
+import Contact from '@pages/contact/Contact';
+import Portfolio from '@pages/portfolio/Portfolio';
+import Blog from '@pages/blog/Blog';
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} /> // This will use the Home component
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+ return (
+   <Router>
+     <div>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/contact" element={<Contact />} />
+         <Route path="/portfolio" element={<Portfolio />} />
+         <Route path="/blog" element={<Blog />} />
+       </Routes>
+     </div>
+   </Router>
+ );
 };
 
 export default App;
