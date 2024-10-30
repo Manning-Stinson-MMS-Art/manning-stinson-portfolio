73b9a19042c30c4ef31b26878f6b000fb1a72ff7
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-30 13:57:32 +0000

    updated footer styles

--- ms-portfolio/src/App.jsx
+++ ms-portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '@pages/home/Home'; // Keep as Home
 import Contact from '@pages/contact/Contact'; 
+import Portfolio from './pages/portfolio/Portfolio';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} /> // This will use the Home component
           <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
       </div>
     </Router>
