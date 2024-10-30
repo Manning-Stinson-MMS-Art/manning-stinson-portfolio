258e197e91f81217ca69a05474f02035a70ec231
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-10-30 16:12:05 +0000

    updated .scss files for a more organzied layout, imports for mixins

--- ms-portfolio/vite.config.js
+++ ms-portfolio/vite.config.js
@@ -5,40 +5,36 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-  
-  server: {
-    port: 8080,
-  },
-  
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-    },
-  },
-  
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "@styles/variables.scss";
-          @import "@styles/app.scss";
-          @import "@styles/mixins.scss";
-          @import "@styles/base.scss";
-          @import "@styles/reset.scss";
-          @import "@styles/fonts.scss";
-          @import "@styles/typography.scss";
-          @import "@styles/utilities.scss";
-          @import "@styles/animations.scss";
-        `,
-      },
-    },
-  },
+ plugins: [react(), svgr()],
+ build: {
+   outDir: 'dist',
+   emptyOutDir: true,
+ },
+ server: {
+   port: 8080,
+ },
+ resolve: {
+   alias: {
+     '@components': path.resolve(__dirname, 'src/components'),
+     '@styles': path.resolve(__dirname, 'src/styles'),
+     '@pages': path.resolve(__dirname, 'src/pages'),
+   },
+ },
+ css: {
+   preprocessorOptions: {
+     scss: {
+       additionalData: `
+         @import "@styles/variables.scss";
+         @import "@styles/mixins.scss";
+         @import "@styles/reset.scss";
+         @import "@styles/base.scss";
+         @import "@styles/typography.scss"; 
+         @import "@styles/utilities.scss";
+         @import "@styles/animations.scss";
+         @import "@styles/fonts.scss";
+         @import "@styles/app.scss";
+       `,
+     },
+   },
+ },
 });
