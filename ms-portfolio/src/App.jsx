8e1b0f991e9aca27e18673563f61213a996c53fe
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-11-05 18:28:35 +0000

    added routes to app.jsx

--- ms-portfolio/src/App.jsx
+++ ms-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import Home from '@pages/home/Home';
 import Contact from '@pages/contact/Contact';
 import Portfolio from '@pages/portfolio/Portfolio';
 import Blog from '@pages/blog/Blog';
+import About from '@pages/about/About';
 
 const App = () => {
  return (
@@ -15,6 +16,7 @@ const App = () => {
          <Route path="/contact" element={<Contact />} />
          <Route path="/portfolio" element={<Portfolio />} />
          <Route path="/blog" element={<Blog />} />
+         <Route path="/about" element={<About />} />
        </Routes>
      </div>
    </Router>
---
+++ ms-portfolio/src/pages/about/About.jsx
@@ -0,0 +1,14 @@
+// Contact.jsx
+import React from 'react';
+import Layout from '@components/layout/Layout';
+import './About.scss';
+
+const About = () => {
+  return (
+    <Layout>
+      <div>About Page</div>
+    </Layout>
+  );
+};
+
+export default About;
