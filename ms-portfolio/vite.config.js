215b3675ae71df8d4f81cee00a57d5705d863443
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-11-06 18:24:05 +0000

    updated to @use, instead of @import for SASS

--- ms-portfolio/vite.config.js
+++ ms-portfolio/vite.config.js
@@ -5,41 +5,32 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
- plugins: [react(), svgr()],
- build: {
-   outDir: 'dist',
-   emptyOutDir: true,
- },
- server: {
-   port: 8080,
- },
- resolve: {
-   alias: {
-     '@components': path.resolve(__dirname, 'src/components'),
-     '@styles': path.resolve(__dirname, 'src/styles'),
-     '@pages': path.resolve(__dirname, 'src/pages'),
-     '@utils': path.resolve(__dirname, './src/utils'),
-     '@hooks': path.resolve(__dirname, './src/hooks'),
-     '@assets': path.resolve(__dirname, './src/assets'),
-
-   },
- },
- css: {
-   preprocessorOptions: {
-     scss: {
-       additionalData: `
-       @import "@styles/tailwind.css";  
-       @import "@styles/variables.scss";
-         @import "@styles/mixins.scss";
-         @import "@styles/reset.scss";
-         @import "@styles/base.scss";
-         @import "@styles/typography.scss"; 
-         @import "@styles/utilities.scss";
-         @import "@styles/animations.scss";
-         @import "@styles/fonts.scss";
-         @import "@styles/app.scss";
-       `,
-     },
-   },
- },
+  plugins: [react(), svgr()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@styles/index" as *;
+          @import "@styles/tailwind.css";
+        `,
+      },
+    },
+  },
 });
