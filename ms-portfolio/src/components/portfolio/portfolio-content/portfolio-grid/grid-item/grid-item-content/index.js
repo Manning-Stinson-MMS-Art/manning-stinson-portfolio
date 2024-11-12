efc9ded8f552b3c0124c348b432e640be22d84da
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-11-12 14:44:06 +0000

    updated portfolio component

---
+++ ms-portfolio/src/components/portfolio/portfolio-content/portfolio-grid/grid-item/grid-item-content/index.js
@@ -0,0 +1,5 @@
+export { default as GridItemContent } from './Index';
+export * from './grid-item-description';
+export * from './grid-item-image';
+export * from './grid-item-tags';
+export * from './grid-item-title';
